c0fe8b213321508098c13cf9b4ceac58cd5820db
Falco Prescher <falcoprescher@googlemail.com>
2016-02-08 21:30:22 +0100

    Add board model and .tern-project settings

---
+++ server/models/Board.js
@@ -0,0 +1,15 @@
+var mongoose     = require('mongoose');
+var Schema       = mongoose.Schema;
+
+var BoardSchema   = new Schema({
+    _id : String,
+    users: [{ type: Schema.Types.ObjectId, ref: 'User' }],
+    title: String,
+    description: String,
+    creator: [{ type: Schema.Types.ObjectId, ref: 'User' }],
+    createdate: { type: Date, default: Date.now },
+    thumbnail: String,
+    tags: { type: [String], index: true }
+});
+
+module.exports = mongoose.model('Board', BoardSchema);
